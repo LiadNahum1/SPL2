5f20975acd6a232f42bc9b0158e0556104d12291
liad <liadna@post.bgu.ac.il>
2018-12-10 23:53:17 +0200

    keep going

--- src/main/java/bgu/spl/mics/application/BookStoreRunner.java
+++ src/main/java/bgu/spl/mics/application/BookStoreRunner.java
@@ -1,10 +1,33 @@
 package bgu.spl.mics.application;
 
-/** This is the Main class of the application. You should parse the input file, 
+import bgu.spl.mics.application.passiveObjects.BookInventoryInfo;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Path;
+
+
+/** This is the Main class of the application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output serialized objects.
  */
 public class BookStoreRunner {
     public static void main(String[] args) {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(args[0]));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Gson gson = new Gson();
+        JsonObject js = gson.fromJson(bufferedReader, JsonObject.class);
+        System.out.println(js.getClass());
+
     }
 }
